1f76d224c9fd7dc4eb83fc93e9108d0ac3204773
Jirayu Klinudom <jizato.jirayu2001@gmail.com>
2024-11-06 16:40:46 +0700

    add the 7.4 ให้สั่งอัพเดตคำที่มีทั้งหมดให้ตัวอักษรตัวแรกเป็นตัวพิมพ์ใหญ่

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from services.services import connect_to_database, create_table, insert_words_into_db, search_word_by_length, search_word_with_same_first_and_last_character
+from services.services import capitalize_the_first_character_of_all_words, connect_to_database, create_table, insert_words_into_db, search_word_by_length, search_word_with_same_first_and_last_character, search_word_with_two_or_more_same_characters
 from utils.utils import load_words_from_csv, create_text_file, get_folder_size_and_report, zip_first_level_directories
 
 def main():
@@ -19,20 +19,24 @@ def main():
     # 5. ทำ report ของ folder size ว่ามีขนาดเท่าไหร่เป็น Kbyte และมีลิสต์ของแต่ละไฟล์ด้วย นึกถึงคำสั่ง ls -l ใน unix และทำเฉพำะ level 1
     get_folder_size_and_report()
 
-
     # 7. เอา dictionary ลงใน Database แบบไหนก็ได้เช่น SqlLite โดยรันแบบ embeded mode  โดยการออกแบบให้สามารถ query เพื่อตอบคำถามเหล่านี้ได้ 
     connection = connect_to_database(database)
     create_table(connection)
     insert_words_into_db(connection, words)
 
-    #7.1 มีคำกี่คำที่มีความยาว > 5 ตัวอักษร
+    # 7.1 มีคำกี่คำที่มีความยาว > 5 ตัวอักษร
     search_word_by_length(connection)
 
-    # 7.2 มีคำกี่คำที่ขึ้นต้นและลงท้ำยด้วยตัวอักษรเดียวกัน
+    # 7.2 มีคำกี่คำที่มีตัวอักษรซ้ำในคำมากกว่าหรือเท่ากับ 2 characters
+    search_word_with_two_or_more_same_characters(connection)
+
+    # 7.3 มีคำกี่คำที่ขึ้นต้นและลงท้ำยด้วยตัวอักษรเดียวกัน
     search_word_with_same_first_and_last_character(connection)
 
+    # 7.4 ให้สั่งอัพเดตคำที่มีทั้งหมดให้ตัวอักษรตัวแรกเป็นตัวพิมพ์ใหญ่
+    capitalize_the_first_character_of_all_words(connection)
 
-    # 8. Export คำใน database ทั้งหมดออกมำเป็น pdf file เรียงบรรทัดละคำ (ขนำดใช้เป็น A4) 
+    # 8. Export คำใน database ทั้งหมดออกมาเป็น pdf file เรียงบรรทัดละคำ (ขนาดใช้เป็น A4) 
 
 
     connection.close()
